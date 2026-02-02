83733ba8066e33b8cd232239ec7b81bdfe3d20b0
Vidya Sagar D <dvsagar2000@gmail.com>
2026-02-02 21:27:44 +0000

    Day 2: Added device query tool and hardware specs

--- kernels/device_query.cu
+++ kernels/device_query.cu
@@ -2,29 +2,19 @@
 #include<cuda.h>
 
 int main(){
-    FILE *filePointer;
-    filePointer = fopen("gpu-inference-kernel/docs/hardware.md", "w");
-
-    if (filePointer == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
-
-
     int devices;
     cudaGetDeviceCount(&devices);
-    fprintf(filePointer, "# Current GPU properties\n");
+    printf("# Current GPU properties\n");
     for(int i=0; i<devices; i+=1){
         cudaDeviceProp prop;
         cudaGetDeviceProperties(&prop, i);
-        fprintf(filePointer, "## Device:- %d\n", i);
-        fprintf(filePointer, "- Device name:- %s\n", prop.name);
-        fprintf(filePointer, "- Number of SMs:- %d\n", prop.multiProcessorCount);
-        fprintf(filePointer, "- Max threads for block:- %d\n", prop.maxThreadsPerBlock);
-        fprintf(filePointer, "- Total Global memory:- %zu\n", prop.totalGlobalMem);
-        fprintf(filePointer, "- Total Shared memory:- %zu\n", prop.sharedMemPerBlock);
-        fprintf(filePointer, "- Warp Size:- %d\n", prop.warpSize);
-        fprintf(filePointer, "\n**************\n");
+        printf("## Device:- %d\n", i);
+        printf("- Device name:- %s\n", prop.name);
+        printf("- Number of SMs:- %d\n", prop.multiProcessorCount);
+        printf("- Max threads for block:- %d\n", prop.maxThreadsPerBlock);
+        printf("- Total Global memory:- %zu\n", prop.totalGlobalMem);
+        printf("- Total Shared memory:- %zu\n", prop.sharedMemPerBlock);
+        printf("- Warp Size:- %d\n", prop.warpSize);
+        printf("\n**************\n");
     }
-    fclose(filePointer);
 }
